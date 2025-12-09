75a74b6be0dac64043cfba1dd5d8a4c2f57da142
Jeffrey Edisah <jeffreyedisah@gmail.com>
2025-12-09 02:42:38 +0100

    added function to count captured seeds

--- awale.js
+++ awale.js
@@ -5,6 +5,7 @@ let player1SeedCount = 0;
 
 const makeAPlay = (holeNumber) => {
     let numberOfSeedsInHole = board[holeNumber];
+    let holeNumberToFill;
     board[holeNumber] = 0;
     while (numberOfSeedsInHole > 0) {
         holeNumberToFill = holeNumber + 1;
@@ -13,6 +14,35 @@ const makeAPlay = (holeNumber) => {
         }
         board[holeNumberToFill] += numberOfSeedsInHole;
         numberOfSeedsInHole--;
+    }
+    checkCapturableSeeds(holeNumberToFill);
+}
+
+const checkCapturableSeeds = (lastHoleNumber) => {
+    let lastHoleToCheck;
+    let currentPlayerNumber;
+    if([0,1,2,3,4,5].includes(lastHoleNumber)) {
+        lastHoleToCheck = 0;
+        currentPlayerNumber = 1;
+    } else {
+        lastHoleToCheck = 6;
+        currentPlayerNumber = 0;
+    }
+
+    let currentHoleToCheck = lastHoleNumber;
+
+    while(currentHoleToCheck >= lastHoleToCheck){
+        currentHoleCount = board[currentHoleToCheck];
+        if (currentHoleCount > 3 || currentHoleCount < 2) {
+            break;
+        }
+        if (currentPlayerNumber === 0){
+            player0SeedCount += currentHoleCount;
+        } else {
+            player1SeedCount += currentHoleCount;
+        }
+        board[currentHoleToCheck] = 0;
+        currentHoleToCheck--;
     }
 }
 
